c32d66453400946d6c80d8083235c7c0141b2a76
karelSoupa <karelsoupa@gmail.com>
2023-03-15 11:03:52 +0000

    Deploying to gh-pages from @ karelSoupa/cnc-web@206b0f3d2e54e2de0b3f7943670513ce82b31139 🚀

---
+++ _astro/Gallery.83e9dcb6.js
@@ -0,0 +1,50 @@
+import{h}from"./hooks.module.62a66d98.js";import{o}from"./jsxRuntime.module.7ba0597d.js";import"./preact.module.5aaf1562.js";const f="/_astro/100_9075.9f3cd9a1.jpg",x="/_astro/100_9078.b1abab32.jpg",I="/_astro/100_9080.cb65f71a.jpg",w="/_astro/100_9081.f60b33ae.jpg",v="/_astro/100_9082.e99532e2.jpg",k="/_astro/100_9084.1dd3042c.jpg",y="/_astro/100_9086.aa53cf46.jpg",N="/_astro/100_9092.9017ef81.jpg",C="/_astro/100_9097.e7a21d1e.jpg",z="/_astro/100_9098.0b5f2a59.jpg",H="/_astro/100_9101.b16a4a2f.jpg",D="/_astro/100_9103.01e43e8b.jpg",G="/_astro/100_9106.e668a1be.jpg",P="/_astro/100_9110.6205a895.jpg",Z="/_astro/100_9111.343849e7.jpg",q="/_astro/100_9113.c199596e.jpg",A="/_astro/100_9115.51bfb105.jpg",B="/_astro/100_9116.4d751121.jpg",E="/_astro/100_9117.77420975.jpg",F="/_astro/20170509_175030.b21d4cdb.jpg",J="/_astro/20170514_142847.752d7545.jpg",K="/_astro/20170514_152934.4c6ef1e7.jpg",L="/_astro/20170514_152955.95112288.jpg",M="/_astro/20170528_193602.56608cf2.jpg",O="/_astro/20170625_105954.003bd183.jpg",Q="/_astro/20170818_174439.89da2e1b.jpg",R="/_astro/20170911_182030.76906f54.jpg",S="/_astro/20170915_171651.17bd27d4.jpg",T="/_astro/20171004_083829.02507954.jpg",U="/_astro/100_9097.e7a21d1e.jpg",V="/_astro/img-20170919-wa0010.bce6b775.jpg",W="/_astro/svarence.ebf80b4d.jpg",c="/_astro/Img_1.46a44de5.webp",m="/_astro/Img_2.48bb79a4.webp",n="/_astro/Img_3.468e84b1.webp",i="/_astro/Img_4.5548479f.webp",e="/_astro/Img_5.575dd2d2.webp",r="/_astro/Img_6.ca345c75.webp",p="/_astro/Img_7.49bb456e.webp",b="/_astro/Img_8.0cd17ab8.webp",d="/_astro/Img_10.3097b86f.webp",l="/_astro/Img_11.e2b65106.webp",j="/_astro/Img_12.27e2cac5.webp",X="/_astro/100_9075.0b9dd468.jpg",Y="/_astro/100_9078.29962018.jpg",$="/_astro/100_9080.94877c5e.jpg",t0="/_astro/100_9081.9f087fd9.jpg",o0="/_astro/100_9082.ca7a4ba6.jpg",s0="/_astro/100_9084.ff17e985.jpg",_0="/_astro/100_9086.4721a182.jpg",g0="/_astro/100_9092.05412783.jpg",a0="/_astro/100_9097.0bdee4c7.jpg",c0="/_astro/100_9098.0f8ac1a0.jpg",m0="/_astro/100_9101.08e22e24.jpg",n0="/_astro/100_9103.42422ff3.jpg",i0="/_astro/100_9106.c537f914.jpg",e0="/_astro/100_9110.be1b953c.jpg",r0="/_astro/100_9111.a4d83f07.jpg",p0="/_astro/100_9113.ddc28662.jpg",b0="/_astro/100_9115.1c0424c6.jpg",d0="/_astro/100_9116.7d88391a.jpg",l0="/_astro/100_9117.e9b830d9.jpg",j0="/_astro/20170509_175030.5ada7f4e.jpg",u0="/_astro/20170514_142847.3b5c1ffa.jpg",h0="/_astro/20170514_152934.abd44d4d.jpg",f0="/_astro/20170514_152955.cef7d420.jpg",x0="/_astro/20170528_193602.18d76886.jpg",I0="/_astro/20170625_105954.7f7047d3.jpg",w0="/_astro/20170818_174439.55beb137.jpg",v0="/_astro/20170911_182030.b9e81404.jpg",k0="/_astro/20170915_171651.43cf39f7.jpg",y0="/_astro/20171004_083829.71ef529b.jpg",N0="/_astro/100_9097.0bdee4c7.jpg",C0="/_astro/img-20170919-wa0010.e31a7673.jpg",z0="/_astro/svarence.83950da9.jpg",_=[f,x,I,w,v,k,y,N,C,z,H,D,G,P,Z,q,A,B,E,F,J,K,L,M,O,Q,R,S,T,U,V,W,c,m,n,i,e,r,p,b,d,l,j],H0=[X,Y,$,t0,o0,s0,_0,g0,a0,c0,m0,n0,i0,e0,r0,p0,b0,d0,l0,j0,u0,h0,f0,x0,I0,w0,v0,k0,y0,N0,C0,z0,c,m,n,i,e,r,p,b,d,l,j],Z0=()=>{const u=H0.map((t,g)=>({thumbnail:t,original:_[g],thumbnailHeight:100})),[a,s]=h(-1);return o("div",{className:"max-w-full",children:[o("div",{className:a>=0?"fullscreen visible":"fullscreen",children:[o("button",{onClick:()=>s(-1),className:"exit",children:"Zavřít"}),o("button",{onClick:()=>s(t=>t+1==_.length?0:t+1),className:"next",children:"Další"}),o("button",{onClick:()=>s(t=>t==0?_.length-1:t-1),className:"prev",children:"Předchozí"}),o("img",{loading:"lazy",alt:"cnc",src:_?.[a]})]}),o("div",{className:"flex flex-row flex-wrap max-w-full gap-3 mt-2",children:u.map((t,g)=>o("img",{alt:"cnc",src:t.thumbnail,style:{maxHeight:200},onClick:()=>s(g),className:"rounded-2xl",loading:"lazy"}))}),o("style",{children:`
+           .fullscreen {
+            display: none;
+           }
+          .fullscreen.visible {
+            display: flex;
+            justify-content: center;
+           
+            align-items: center;
+            width: 100vw;
+            height: 100vh;
+            z-index: 2000;
+            top:0;
+            left:0;
+            position: fixed;
+            background: black;
+          }
+         .fullscreen.visible img {
+           max-height: 80%;
+          }
+          .prev {
+            top:50vh;
+            left:20px;
+            position: fixed;
+            color: white;
+           background: red;
+            padding: 10px;
+            border-radius: 16px;
+          }
+          .next {
+            top:50vh;
+            right:20px;
+            position: fixed;
+            color: white;
+            background: red;
+            padding: 10px;
+            border-radius: 16px;
+
+          }
+          .exit {
+            top: 20px;
+            right: 20px;
+            position: fixed;
+            color: white;
+            background: red;
+            padding: 10px;
+            border-radius: 16px;
+          }
+
+        `})]})};export{Z0 as default};
